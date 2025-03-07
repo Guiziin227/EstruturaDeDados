bb700fd35eaa930b8e1b411e377706e2a75b4b59
Guilherme Weber <104660667+Guiziin227@users.noreply.github.com>
2025-03-07 10:07:10 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,60 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+Construir um programa que receba, via teclado do uysuario, n nomes de pessoas.
+1) não pode haver nomes repitidos -- foi
+2) ordernar -- foi
+3) adicionar o usuario, pesquisar se ele esta la
+4) mostrar -- foi
+ */
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        ArrayList<String>list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        char escolha;
+        String nome;
+
+        do {
+
+            System.out.println("Digite um nome:");
+            nome = input.nextLine();
+           if (!list.contains(nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase())) {
+                    list.add(nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase());
+            } else {
+                System.out.println("Nome já existe na lista");
+            }
+
+            System.out.println("Você quer adicionar outro nome? (s/n)");
+            escolha = input.next().charAt(0);
+            input.nextLine();
+
+            list.sort(null);
+
+        }while(escolha != 'n' && escolha != 'N');
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
+
+        System.out.println("Deseja excluir alguém da lista? (s/n)");
+        escolha = input.next().charAt(0);
+        input.nextLine();
+
+        if (escolha != 'n') {
+            System.out.println("Digite o nome que deseja excluir:");
+            nome = input.nextLine();
+            list.remove(nome.substring(0, 1).toUpperCase() + nome.substring(1).toLowerCase());
+        }
+
+        for (String n : list) {
+            System.out.println(n);
+        }
+
     }
 }
